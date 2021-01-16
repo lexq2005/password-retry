e1b8ff15e9381fd1bef8201e59ea69c679538264
Xuequn Le <LEXUEQUN@gmail.com>
2021-01-15 18:49:18 -0700

    3rd edition

--- password.py
+++ password.py
@@ -1,10 +1,10 @@
 password = 'a123456'
 i = 3
 while i > 0:
+	i = i - 1
 	ps = input("Enter Password: ")
 	if ps == password:
 		print('password is correct')
 		break #Exit
 	else:
-		i = i - 1
 		print('Password Error, you have ', i, 'time(s) to try.')
